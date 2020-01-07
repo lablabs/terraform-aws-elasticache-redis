99f72e09f94a36c4522ba5f175a539982bf6b075
Martin <hauskrechtmartin@gmail.com>
2020-01-07 15:53:34 +0100

    Add custom security group

--- main.tf
+++ main.tf
@@ -9,31 +9,6 @@ module "label" {
   tags       = var.tags
 }
 
-#
-# Security Group Resources
-#
-resource "aws_security_group" "default" {
-  count  = var.enabled ? 1 : 0
-  vpc_id = var.vpc_id
-  name   = module.label.id
-
-  ingress {
-    from_port       = var.port # Redis
-    to_port         = var.port
-    protocol        = "tcp"
-    security_groups = var.security_groups
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = module.label.tags
-}
-
 locals {
   elasticache_subnet_group_name = var.elasticache_subnet_group_name != "" ? var.elasticache_subnet_group_name : join("", aws_elasticache_subnet_group.default.*.name)
 }
@@ -71,7 +46,7 @@ resource "aws_elasticache_replication_group" "default" {
   availability_zones            = slice(var.availability_zones, 0, var.cluster_size)
   automatic_failover_enabled    = var.automatic_failover
   subnet_group_name             = local.elasticache_subnet_group_name
-  security_group_ids            = [join("", aws_security_group.default.*.id)]
+  security_group_ids            = var.security_groups
   maintenance_window            = var.maintenance_window
   notification_topic_arn        = var.notification_topic_arn
   engine_version                = var.engine_version
--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "id" {
   description = "Redis cluster ID"
 }
 
-output "security_group_id" {
-  value       = join("", aws_security_group.default.*.id)
-  description = "Security group ID"
-}
-
 output "port" {
   value       = var.port
   description = "Redis port"
